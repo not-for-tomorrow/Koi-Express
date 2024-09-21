12133eb6e801b5a2f76f686864ef18ac703d85ed
Koi Express <nhathuyn1100@gmail.com>
2024-09-21 07:45:23 +0700

    Add Koi-Express project

---
+++ BE/Koi-Express/src/main/java/com/koi_express/KoiExpressApplication.java
@@ -0,0 +1,13 @@
+package com.koi_express;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoiExpressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KoiExpressApplication.class, args);
+	}
+
+}
---
+++ BE/Koi-Express/src/test/java/com/koi_express/KoiExpressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koi_express;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoiExpressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
