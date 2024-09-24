ec5dfba9298049d363612c3e90d53666aed82a64
NTT <thanhtai10903@gmail.com>
2024-09-24 23:46:02 +0700

    Create JWT

---
+++ BE/Koi-Express/src/main/java/com/koi_express/dto/response/AuthenticationResponse.java
@@ -0,0 +1,15 @@
+package com.koi_express.dto.response;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class AuthenticationResponse {
+
+    boolean authenticated;
+    String token;
+}
