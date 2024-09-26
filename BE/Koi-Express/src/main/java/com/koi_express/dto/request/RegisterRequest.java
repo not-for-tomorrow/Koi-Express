203805f04a8de12c07fd9053d0b639dec8e4c039
NTT <thanhtai10903@gmail.com>
2024-09-27 00:09:53 +0700

    Update Register

--- BE/Koi-Express/src/main/java/com/koi_express/dto/request/RegisterRequest.java
+++ BE/Koi-Express/src/main/java/com/koi_express/dto/request/RegisterRequest.java
@@ -13,6 +13,8 @@ import lombok.experimental.FieldDefaults;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class RegisterRequest {
 
+    String fullName;
+
     @NotEmpty(message = "Phone number cannot be empty")
     @Size(min = 10, max = 10, message = "Phone number must consist of exactly 10 digits")
     @Pattern(regexp = "\\d{10}", message = "Phone number must contain exactly 10 digits and only digits")
