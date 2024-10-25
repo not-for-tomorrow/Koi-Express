1a89a2ab391e414349b5bf225772d67c7758f79d
Nguyễn Nhất Huy <116622843+not-for-tomorrow@users.noreply.github.com>
2024-10-25 08:15:55 +0700

    Update main.jsx

--- FE/Koi-Express/src/main.jsx
+++ FE/Koi-Express/src/main.jsx
@@ -1,2 +1,13 @@
-import {DevSupport} from "@react-buddy/ide-toolbox";
-import {ComponentPreviews, useInitial} from "./dev/index.js";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
